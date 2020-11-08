a43297bd818337221201c8f8860f561091a03c2f
tsubasa.masuda <tsubasa.watanabe.19820821@gmail.com>
2020-11-08 08:28:54 +0000

    不要な処理を削除

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -20,8 +20,6 @@ class MicropostsController < ApplicationController
     redirect_back(fallback_location: root_path)
   end
   
-  # => Cache-Control: no-cache
-
   private
 
   def micropost_params
