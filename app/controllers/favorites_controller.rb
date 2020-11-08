abd341a6268dfc3d0c184d7b093cc00db0116b88
tsubasa.masuda <tsubasa.watanabe.19820821@gmail.com>
2020-11-08 08:36:44 +0000

    不要な処理を削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,7 +5,6 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.like(micropost)
     flash[:success] = 'お気に入り登録しました。'
-    #redirect_to current_user
     redirect_back(fallback_location: root_path)
   end
 
@@ -13,7 +12,6 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.unlike(micropost)
     flash[:success] = 'お気に入り登録を解除しました。'
-    #redirect_to current_user
     redirect_back(fallback_location: root_path)
   end
   
